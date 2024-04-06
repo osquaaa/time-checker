cd7a76fec6769eade361152fa8ab811fd979a78d
osquaaa <osquaa@gmail.com>
2024-04-06 16:41:09 +0300

    1

---
+++ js/main2.js
@@ -0,0 +1,28 @@
+const year = document.querySelector('#year');
+const days = document.querySelector('#days');
+const hours = document.querySelector('#hours');
+const minutes = document.querySelector('#minutes');
+const seconds = document.querySelector('#seconds');
+
+const currentYear = new Date().getFullYear();
+const nextYear = new Date(`June 08 ${currentYear} 00:00:00`);
+
+year.innerText = currentYear;
+
+function updateCounter() {
+    const currentTime = new Date();
+    const diff = nextYear - currentTime;
+    
+    const daysLeft = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const hoursLeft = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const minutesLeft = Math.floor(diff / 1000 / 60) % 60;
+    const secondsLeft = Math.floor(diff / 1000) % 60;
+    
+    days.innerText = daysLeft < 10 ? '0' + daysLeft : daysLeft;
+    hours.innerText = hoursLeft < 10 ? '0' + hoursLeft : hoursLeft;
+    minutes.innerText = minutesLeft < 10 ? '0' + minutesLeft : minutesLeft;
+    seconds.innerText = secondsLeft < 10 ? '0' + secondsLeft : secondsLeft;
+}
+updateCounter();
+
+setInterval(updateCounter, 1000);
